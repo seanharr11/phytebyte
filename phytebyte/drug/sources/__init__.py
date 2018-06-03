bffb01079314dd0c5eb9a4b3342987ef0fa0f21f
Sean Harrington <seanharr11@gmail.com>
2018-06-03 12:54:57 -0400

    Sample __all__ usage w/ ABC in drug.sources

---
+++ phytebyte/drug/sources/__init__.py
@@ -0,0 +1,3 @@
+from .base import DrugSource
+
+__all__ = ['DrugSource']
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(name='phytebyte',
+      packages=find_packages(),
+      zip_safe=False,
+      version='1.0',
+      description='',
+      author='Sean Harrington',
+      author_email='seanharr11@gmail.com',
+      setup_requires=['pytest-runner'],
+      tests_require=['pytest'],
+      url='',
+      )
