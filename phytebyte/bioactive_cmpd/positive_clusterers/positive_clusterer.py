9d2b66f6774f4fe55a756940c9f97a43a8098149
Sean Harrington <seanharr11@gmail.com>
2018-07-01 09:16:29 -0400

    Stub class for positive_clusterer, and implemented 'CmpdFingerprintCluster', which will be consumed by modules downstream from PositiveClusterer

---
+++ phytebyte/bioactive_cmpd/positive_clusterers/positive_clusterer.py
@@ -0,0 +1,7 @@
+from .cluster import CmpdFingerprintCluster
+from typing import List
+
+
+class PositiveFingerprintClusterer():
+    def cluster(self, *args, **kwargs) -> List[CmpdFingerprintCluster]:
+        pass
