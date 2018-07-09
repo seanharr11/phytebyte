9a9361917ff47f74ee533907fd47e5c363a79c2c
Sean Harrington <seanharr11@gmail.com>
2018-07-09 16:59:44 -0400

    Begun the process of Inverting Dependency between NegativeSampler, and Fingerprinter, supporting any encoding (i.e. numpy, tensor, etc..)

--- phytebyte/bioactive_cmpd/negative_samplers/base.py
+++ phytebyte/bioactive_cmpd/negative_samplers/base.py
@@ -2,7 +2,6 @@ from abc import abstractmethod, ABC
 from multiprocessing import cpu_count, Pool
 from typing import List, Iterator
 
-
 from phytebyte.bioactive_cmpd.sources.base import BioactiveCompoundSource
 from phytebyte.fingerprinters.base import Fingerprinter
 
@@ -15,24 +14,25 @@ class NegativeSampler(ABC):
     def __init__(self,
                  source: BioactiveCompoundSource,
                  fingerprinter: Fingerprinter,
+                 neg_sample_encoding: str,
                  num_proc: int=cpu_count(),
                  *args, **kwargs):
         self._source = source
         self._fingerprinter = fingerprinter
+        self._neg_sample_encoding = neg_sample_encoding
         self._num_proc = num_proc
-        self._excluded_mol_bitarrays = None
+        self._excluded_mol_ls = None
 
     def sample(self, excluded_smiles_ls: List[str], sz: int) -> Iterator:
         rand_neg_smiles_iter = self._source.fetch_random_compounds_exc_smiles(
             excluded_smiles=excluded_smiles_ls,
             limit=sz * 2)
-        self._excluded_mol_bitarrays = [
-            self._fingerprinter.smiles_to_bitarray(smiles)
-            for smiles in excluded_smiles_ls]
+        self._excluded_mol_ls = self._encode_excluded_mol_ls(
+            excluded_smiles_ls)
         with Pool(self._num_proc) as p:
             cnt = 0
             for neg_x in p.imap_unordered(
-               self._filter, rand_neg_smiles_iter):
+               self._filter_and_convert, rand_neg_smiles_iter):
                 if neg_x is not None:
                     cnt += 1
                     if cnt > sz:
@@ -46,9 +46,30 @@ class NegativeSampler(ABC):
                 raise NotEnoughSamples(
                     f"Queried {sz*2} samples, filterd to {cnt}, expected {sz}")
 
+    def _filter_and_encode(self, neg_smiles: str):
+        return self.dispatched_fp_encoding_func(neg_smiles)\
+            if self._filter_func(neg_smiles) else None
+
+    # TODO: @lazy_property
+    def _dispatched_fp_encoding_func(self, encoding: str):
+        if encoding == 'numpy':
+            return self._fingerprinter.smiles_to_nparray
+        elif encoding == 'bitarray':
+            return self._fingerprinter.smiles_to_bitarray
+        else:
+            raise NotImplementedError
+
     @abstractmethod
-    def _filter(self, smiles: str):
+    def _filter_func(self, smiles: str) -> bool:
         """ Params: smiles :str - The SMiLES (str) representation of the cmpd
-            Returns: np.ndarray representing the Fingerprinter-encoded repr
+            Returns: Whether or not to include the SMiLES cmpd as neg sample
+        """
+        pass
+
+    @abstractmethod
+    def _encode_excluded_mol_ls(self, excluded_smiles: List[str]) -> List:
+        """ Convert the `excluded_smiles` list into whatever encoding is
+        required to be accessed within the concurrent `filter_and_encode`
+        step, and it's call to _filter_func()`
         """
         pass
--- phytebyte/bioactive_cmpd/negative_samplers/tanimoto_thresh.py
+++ phytebyte/bioactive_cmpd/negative_samplers/tanimoto_thresh.py
@@ -1,5 +1,5 @@
 from bitarray import bitarray
-import numpy as np
+from typing import List
 
 from .base import NegativeSampler
 
@@ -12,14 +12,19 @@ class TanimotoThreshNegativeSampler(NegativeSampler):
         self._max_tanimoto_thresh = max_tanimoto_thresh
         super().__init__(*args, **kwargs)
 
-    def _filter(self, neg_smile: str) -> np.ndarray:
+    def _encode_excluded_mol_ls(self,
+                                excluded_smiles: List[str]) -> List[bitarray]:
+        encoding_func = self._dispatched_fp_encoding_func('bitarray')
+        return [encoding_func(smiles) for smiles in excluded_smiles]
+
+    def _filter_func(self, neg_smile: str) -> bool:
         neg_smile_bitarray = self._fingerprinter.smiles_to_bitarray(neg_smile)
-        for excluded_mol_bitarray in self._excluded_mol_bitarrays:
+        for excluded_mol_bitarray in self._excluded_mol_ls:
             tani = self._calculate_tanimoto(excluded_mol_bitarray,
                                             neg_smile_bitarray)
             if tani > self._max_tanimoto_thresh:
-                return None
-        return self._fingerprinter.bitarray_to_nparray(neg_smile_bitarray)
+                return False
+        return True
 
     @staticmethod
     def _calculate_tanimoto(left_bitarr: bitarray,
