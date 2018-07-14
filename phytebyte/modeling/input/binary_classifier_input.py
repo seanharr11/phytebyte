3e9afdcbef784d81196409517fd00493da699063
kwesterman <ken.e.westerman@gmail.com>
2018-07-14 14:30:25 +0200

    Clean up binary_classifier_input and add unit tests.

--- phytebyte/modeling/input/binary_classifier_input.py
+++ phytebyte/modeling/input/binary_classifier_input.py
@@ -25,24 +25,29 @@ class BinaryClassifierInput(ABC):
 
 class NumpyBinaryClassifierInput(BinaryClassifierInput):
     def __init__(self, positives: np.ndarray, negatives: np.ndarray):
-        self._X = np.append(self.positives, self.negatives)
-        self._pos_idx = np.arange(len(self.positives))
-        self._neg_idx = np.arange(len(self.positives), len(self.negatives))
-        self._y = np.append(np.ones(len(self.positives)),
-                            np.zeroes(len(self.negatives)))
+        self._X = np.append(positives, negatives, axis=0)
+        self._pos_idx = np.arange(len(positives))
+        self._neg_idx = np.arange(len(positives), len(negatives))
+        self._y = np.append(np.ones(len(positives)),
+                            np.zeros(len(negatives)))
         self._train_idx = None
         self._test_idx = None
 
     def split(self, test_size=.3, rand_state_seed=100) -> None:
-        self._train_idx, self._test_idx = ShuffleSplit(
-            n_splits=1,
-            test_size=test_size,
-            random_state=rand_state_seed).split(self._X)
+        ss = ShuffleSplit(n_splits=1, test_size=test_size)
+        self._train_idx, self._test_idx = next(
+            ShuffleSplit(
+                n_splits=1,
+                test_size=test_size,
+                random_state=rand_state_seed)
+            .split(self._X))
 
     @property
-    def train(self) -> np.ndarray:
-        return self._X[self._train_idx]
+    def train(self) -> tuple:
+        return (self._X[self._train_idx],
+                self._y[self._train_idx])
 
     @property
-    def test(self) -> np.ndarray:
-        return self._X[self._test_idx]
+    def test(self) -> tuple:
+        return (self._X[self._test_idx],
+                self._y[self._test_idx])
