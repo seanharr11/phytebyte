2bc56889ddf4f7ea20344dfd5a28ca807919db7e
Sean Harrington <seanharr11@gmail.com>
2018-07-28 18:05:47 -0400

    alpha_run uses RedisCache

--- notebooks/alpha_run.py
+++ notebooks/alpha_run.py
@@ -4,16 +4,15 @@ from phytebyte.food_cmpd.sources.foodb import FoodbFoodCmpdSource
 from phytebyte.bioactive_cmpd.sources import ChemblBioactiveCompoundSource
 from phytebyte.bioactive_cmpd.target_input import GeneTargetsInput
 from phytebyte.fingerprinters import Fingerprinter
-from phytebyte.cache import DictEncodedSmilesCache
+from phytebyte.cache import RedisEncodedSmilesCache
 
 import os
 
 
 chembl_db_url = os.environ['CHEMBL_DB_URL']
 source = ChemblBioactiveCompoundSource(chembl_db_url)
-cache = DictEncodedSmilesCache()
-cache.load('daylight', 'numpy')
-cache.load('daylight', 'bitarray')
+cache = RedisEncodedSmilesCache()
+cache.connect("", "")
 print("Loaded cache.")
 target_input = GeneTargetsInput(['HMGCR'])
 
@@ -25,6 +24,7 @@ pb.set_fingerprinter("daylight", cache)
 f1_scores = pb.evaluate_models('Random Forest',
                                neg_sample_size_factor=1,
                                true_threshold=.8)
+"""
 food_cmpd_source = FoodbFoodCmpdSource(os.environ['FOODB_URL'])
 food_cmpds_sorted = pb.sort_predicted_bioactive_food_cmpds(food_cmpd_source)
 for i, (food_cmpd, score) in enumerate(food_cmpds_sorted[:100]):
@@ -32,3 +32,4 @@ for i, (food_cmpd, score) in enumerate(food_cmpds_sorted[:100]):
                                             ignore_if_no_food_content=True)
     if food_info:
         print(f"{i}. ({score}) {food_info}")
+"""
