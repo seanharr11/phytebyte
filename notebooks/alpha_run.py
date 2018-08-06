6a9c8f0a300d8028c7fd51211765b4f4e02ca6d5
Sean Harrington <seanharr11@gmail.com>
2018-08-05 21:51:07 -0400

    Added alpha_run to use new cache'ing strategy, (right now cache=None is default)

--- notebooks/alpha_run.py
+++ notebooks/alpha_run.py
@@ -4,16 +4,17 @@ from phytebyte.food_cmpd.sources.foodb import FoodbFoodCmpdSource
 from phytebyte.bioactive_cmpd.sources import ChemblBioactiveCompoundSource
 from phytebyte.bioactive_cmpd.target_input import GeneTargetsInput
 from phytebyte.fingerprinters import Fingerprinter
-from phytebyte.cache import RedisEncodedSmilesCache
+from phytebyte.cache import BitstringSmilesCache
 
 import os
 
 
 chembl_db_url = os.environ['CHEMBL_DB_URL']
 source = ChemblBioactiveCompoundSource(chembl_db_url)
-cache = RedisEncodedSmilesCache()
-cache.connect("", "")
-print("Loaded cache.")
+
+# cache = BitstringSmilesCache.create("json")
+# cache.load()
+cache = None
 target_input = GeneTargetsInput(['HMGCR'])
 
 pb = PhyteByte(source, target_input)
@@ -22,14 +23,13 @@ pb.set_positive_clusterer('doesnt matter still',
                           Fingerprinter.create('daylight', cache))
 pb.set_fingerprinter("daylight", cache)
 f1_scores = pb.evaluate_models('Random Forest',
-                               neg_sample_size_factor=1,
+                               neg_sample_size_factor=1000,
                                true_threshold=.8)
-"""
 food_cmpd_source = FoodbFoodCmpdSource(os.environ['FOODB_URL'])
 food_cmpds_sorted = pb.sort_predicted_bioactive_food_cmpds(food_cmpd_source)
+print("Classifying Food Compounds...")
 for i, (food_cmpd, score) in enumerate(food_cmpds_sorted[:100]):
     food_info = food_cmpd.get_food_info_str(ignore_if_no_food=True,
                                             ignore_if_no_food_content=True)
     if food_info:
         print(f"{i}. ({score}) {food_info}")
-"""
